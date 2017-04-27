00fa047e26ef9e4fa545990fa29fe8a648ab8ca9
mahedley <mahedley@microsoft.com>
2017-04-27 00:13:36 -0700

    Update Script

--- mongodb-nodejs-high-availability/Script/templateScript.sh
+++ mongodb-nodejs-high-availability/Script/templateScript.sh
@@ -9,8 +9,8 @@ sudo apt-get -y install nodejs
 sudo apt-get -y install npm
 sudo ln -s /usr/bin/nodejs /usr/bin/node
 sudo npm install pm2 -g
-git clone -b clusterqs https://github.com/mahedley/ToDo_MEAN.git
-chmod -R 644 /ToDo_Mean
+sudo git clone -b clusterqs https://github.com/mahedley/ToDo_MEAN.git
+sudo chmod -R 644 /ToDo_Mean
 cd /ToDo_Mean
 sudo npm install
 sudo pm2 start npm -- start
