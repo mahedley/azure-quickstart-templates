eb0611ec43830e13d1c89a78499d3da37cf0fb86
mahedley <mahedley@microsoft.com>
2017-04-27 00:18:50 -0700

    Updated

--- mongodb-nodejs-high-availability/Script/templateScript.sh
+++ mongodb-nodejs-high-availability/Script/templateScript.sh
@@ -11,8 +11,8 @@ sudo apt-get -y install git
 sudo ln -s /usr/bin/nodejs /usr/bin/node
 sudo npm install pm2 -g
 sudo git clone -b clusterqs https://github.com/mahedley/ToDo_MEAN.git
-sudo chmod -R 644 /ToDo_Mean
-cd /ToDo_Mean
+sudo chmod -R 655 /ToDo_MEAN
+cd /ToDo_MEAN
 sudo npm install
 sudo pm2 start npm -- start
-env PATH=$PATH:/usr/local/bin pm2 startup -u root
+sudo env PATH=$PATH:/usr/local/bin pm2 startup -u root
